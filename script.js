41d9e16e6be9a074587e31aefca6706e872f2e0c
EGJJR <josepheddjr@gmail.com>
2022-06-14 19:58:19 -0400

    Finished

--- script.js
+++ script.js
@@ -2,7 +2,19 @@
 let image 
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize 
+let changeSize
+document.querySelector('button').onclick = function(){
+  document.querySelector('img').style.width = '250px';
+  if (image.style.height == "100px") {
+    image.style.height = "25px"
+  }
+  else {
+    image.style.height = "100px"
+  }
+}
+
+
+
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
